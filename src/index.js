7f4858a6500518fea6ebb4673f25314ba2c1a13f
Aleksandra <ujvari65@gmail.com>
2026-01-07 12:53:58 +0100

    Fix testimonials images: moved to src/assets/testimonials and updated imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderujemo App
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
